98b7839bab37b2be0c2e6bd17b743b0fccab472a
KyanChen <keyan1055002755@163.com>
2021-12-22 16:20:43 +0800

    change

--- configs/yolo/yolov4_d53_mstrain_256_300e_coco.py
+++ configs/yolo/yolov4_d53_mstrain_256_300e_coco.py
@@ -1,7 +1,7 @@
-checkpoint_config = dict(interval=5)
+checkpoint_config = dict(interval=20)
 # yapf:disable
 log_config = dict(
-    interval=2,
+    interval=200,
     hooks=[
         dict(type='TextLoggerHook'),
         dict(type='TensorboardLoggerHook')
@@ -11,7 +11,7 @@ custom_hooks = [dict(type='NumClassCheckHook')]
 
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-load_from = 'results/test/latest.pth'
+load_from = None
 resume_from = None
 workflow = [('train', 1)]
 
@@ -29,7 +29,7 @@ model = dict(
         out_channels=[512, 256, 128]),
     bbox_head=dict(
         type='YOLOV4Head',
-        num_classes=3,
+        num_classes=1,
         in_channels=[512, 256, 128],
         out_channels=[1024, 512, 256],
         anchor_generator=dict(
@@ -37,11 +37,14 @@ model = dict(
             # base_sizes=[[(116, 90), (156, 198), (373, 326)],
             #             [(30, 61), (62, 45), (59, 119)],
             #             [(10, 13), (16, 30), (33, 23)]],
-            base_size=[
-                [[30, 28], [33, 17], [21, 25]],
-                [[24, 17], [18, 17], [14, 21]],
-                [[20, 12], [12, 15], [11, 11]]
-                       ],
+            # base_sizes=[
+            #     [[30, 28], [33, 17], [21, 25]],
+            #     [[24, 17], [18, 17], [14, 21]],
+            #     [[20, 12], [12, 15], [11, 11]]],
+            base_sizes=[
+                [[2*30, 2*28], [2*33, 2*17], [2*21, 2*25]],
+                [[2*24, 2*17], [2*18, 2*17], [2*14, 2*21]],
+                [[2*20, 2*12], [2*12, 2*15], [2*11, 2*11]]],
             strides=[32, 16, 8]),
         bbox_coder=dict(type='YOLOBBoxCoder'),
         featmap_strides=[32, 16, 8],
@@ -65,15 +68,15 @@ model = dict(
     train_cfg=dict(
         assigner=dict(
             type='GridAssigner',
-            pos_iou_thr=0.5,
-            neg_iou_thr=0.5,
+            pos_iou_thr=0.3,
+            neg_iou_thr=0.3,
             min_pos_iou=0)),
     test_cfg=dict(
         nms_pre=1000,
         min_bbox_size=0,
         score_thr=0.05,
         conf_thr=0.005,
-        nms=dict(type='nms', iou_threshold=0.45),
+        nms=dict(type='nms', iou_threshold=0.4),
         max_per_img=100))
 # dataset settings
 dataset_type = 'MyCocoDataset'
@@ -85,19 +88,24 @@ img_norm_cfg = dict(mean=[52.27434974492982, 69.82640643452488, 79.0174495833688
 train_pipeline = [
     dict(type='LoadImageFromFile', to_float32=True),
     dict(type='LoadAnnotations', with_bbox=True),
-    # dict(
-    #     type='Expand',
-    #     mean=img_norm_cfg['mean'],
-    #     to_rgb=img_norm_cfg['to_rgb'],
-    #     ratio_range=(1, 2)),
-    # dict(
-    #     type='MinIoURandomCrop',
-    #     min_ious=(0.4, 0.5, 0.6, 0.7, 0.8, 0.9),
-    #     min_crop_size=0.3),
+    dict(
+        type='Expand',
+        mean=img_norm_cfg['mean'],
+        to_rgb=img_norm_cfg['to_rgb'],
+        ratio_range=(1, 1.4)),
+    dict(
+        type='MinIoURandomCrop',
+        min_ious=(0.01, 0.05, 0.1),
+        min_crop_size=0.7),
     # dict(type='Resize', img_scale=[(320, 320), (608, 608)], keep_ratio=True),
     dict(type='Resize', img_scale=[(256, 256)], keep_ratio=False),
-    # dict(type='RandomFlip', flip_ratio=0.5),
-    # dict(type='PhotoMetricDistortion'),
+    dict(type='RandomFlip', flip_ratio=0.5, direction=['horizontal', 'vertical']),
+    dict(type='PhotoMetricDistortion',
+         brightness_delta=20,
+         contrast_range=(0.7, 1.3),
+         saturation_range=(0.7, 1.3),
+         hue_delta=15
+         ),
     dict(type='Normalize', **img_norm_cfg),
     # dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
@@ -126,19 +134,19 @@ data = dict(
     workers_per_gpu=1,
     train=dict(
         type=dataset_type,
-        ann_file='../../data/tiny_ship/tiny_train.json',
+        ann_file='../data/tiny_ship/tiny_train.json',
         img_prefix=data_root+'/train',
         classes=class_name,
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
-        ann_file='../../data/tiny_ship/tiny_val.json',
+        ann_file='../data/tiny_ship/tiny_val.json',
         img_prefix=data_root+'/val',
         classes=class_name,
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file='../../data/tiny_ship/tiny_test.json',
+        ann_file='../data/tiny_ship/tiny_test.json',
         classes=class_name,
         img_prefix=data_root+'/test',
         pipeline=test_pipeline))
@@ -147,6 +155,7 @@ data = dict(
 # optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
 optimizer = dict(type='AdamW', lr=0.01, betas=(0.9, 0.999), eps=1e-8, weight_decay=1e-2)
 # optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+optimizer_config = None
 # learning policy
 # lr_config = dict(
 #     policy='PolyLrUpdaterHook',
